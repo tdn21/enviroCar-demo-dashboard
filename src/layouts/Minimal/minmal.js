41d65e7dbc0fe24cad3c5fd7a64e315a4556c8f3
Tarun215 <tarun.cer18@itbhu.ac.in>
2020-04-05 23:40:04 +0530

    linked all components of minimal layout

---
+++ src/layouts/Minimal/minmal.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/styles';
+import { useMediaQuery } from '@material-ui/core';
+
+import Topbar from './components/topbar/topbar';
+import Footer from './components/footer/footer';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingTop: 56,
+    height: '100%',
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: 64
+    }
+  },
+  shiftContent: {
+    paddingLeft: 240
+  },
+  content: {
+    height: '100%'
+  }
+}));
+
+const Main = props => {
+  const { children } = props;
+
+  const classes = useStyles();
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('lg'), {
+    defaultMatches: true
+  });
+
+  return (
+    <div
+      className={clsx({
+        [classes.root]: true,
+        [classes.shiftContent]: isDesktop
+      })}
+    >
+      <Topbar />
+      <main className={classes.content}>
+        {children}
+        <Footer />
+      </main>
+    </div>
+  );
+};
+
+Main.propTypes = {
+  children: PropTypes.node
+};
+
+export default Main;
