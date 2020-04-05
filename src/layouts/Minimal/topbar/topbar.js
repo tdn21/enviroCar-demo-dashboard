c1b93277403c5ebc14290c5d1cc1af3c9e7aa4bc
Tarun215 <tarun.cer18@itbhu.ac.in>
2020-04-05 23:35:15 +0530

    added topbar to minimal layout

---
+++ src/layouts/Minimal/topbar/topbar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { AppBar, Toolbar, Hidden, IconButton, Button } from '@material-ui/core';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    boxShadow: 'none'
+  },
+  flexGrow: {
+    flexGrow: 1
+  },
+  signOutButton: {
+    marginLeft: theme.spacing(1)
+  }
+}));
+
+const Topbar = props => {
+  const { className, onSidebarOpen, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <AppBar
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <Toolbar>
+        <RouterLink to="/">
+          <img
+            style={{height:50}}
+            alt="Logo"
+            src="/images/logos/logo_white.png"
+          />
+        </RouterLink>
+        <div className={classes.flexGrow} />
+        <Hidden mdDown>
+            <Button color="inherit" variant="text">Munster <ArrowDropDownIcon /></Button>
+        </Hidden>
+        <Hidden lgUp>
+          <IconButton
+            color="inherit"
+            onClick={onSidebarOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+Topbar.propTypes = {
+  className: PropTypes.string,
+  onSidebarOpen: PropTypes.func
+};
+
+export default Topbar;
